0bf3301478be474642c3c0bd03ad6f06294f09e0
devgabe1 <129017361+devgabe1@users.noreply.github.com>
2023-03-29 01:48:09 -0300

    Adicionado exercício 17 da lista 1

---
+++ src/lista_de_exercícios_1/17_una_eletronica.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+int main (){
+setlocale(LC_ALL, "portuguese");
+
+float votos_c1, votos_c2, votos_c3, votos_nulos, votos_brancos, eleitores, votos_validos;
+char candidato_1[50], candidato_2[50], candidato_3[50];
+
+printf ("Bem vinda a Urna eletrônica!\n\n\n");
+
+printf ("Insira o nome do Candidato 1: ");
+gets (candidato_1);
+printf ("Bem vindo as eleições %s!\n\n", candidato_1);
+
+
+printf ("Insira o nome do Candidato 2:");
+gets (candidato_2);
+printf ("Bem vindo as eleições %s!\n\n", candidato_2);
+
+printf ("Insira o nome do Candidato 3:");
+gets (candidato_3);
+printf("Bem vindo as eleições %s!\n\n\n", candidato_3);
+
+
+printf ("Insira a quantidade de votos que o candidato %s recebeu:", candidato_1);
+scanf ("%f", &votos_c1);
+
+printf ("\nInsira a quantidade de votos que o candidato %s recebeu:", candidato_2);
+scanf ("%f", &votos_c2);
+
+printf ("\nInsira a quantidade de votos que o candidato %s recebeu:", candidato_3);
+scanf ("%f", &votos_c3);
+
+printf ("\nInsira a quantidade de votos nulos:");
+scanf ("%f", &votos_nulos);
+
+printf ("\nInsira a quantidade de votos em branco:");
+scanf ("%f", &votos_brancos);
+
+
+eleitores = votos_c1 + votos_c2 + votos_c3 + votos_nulos + votos_brancos;
+votos_validos = votos_c1 + votos_c2 + votos_c3;
+
+
+printf ("\n\n\nNúmero total de eleitores: %.f", eleitores);
+
+printf ("\nPercentual de votos válidos: %.2f%%", votos_validos / eleitores * 100);
+
+printf ("\nPercentual de votos para o candidato %s: %.2f%%", candidato_1, votos_c1 / eleitores *100);
+
+printf ("\nPercentual de votos para o candidato %s: %.2f%%", candidato_2, votos_c2 / eleitores *100);
+
+printf ("\nPercentual de votos para o candedato %s: %.2f%%", candidato_3, votos_c3 / eleitores *100);
+
+
+printf ("\n\nPercentual de votos nulos: %.2f%%", votos_nulos / eleitores * 100);
+
+printf ("\n\nPercentual de votos em branco: %.2f%%", votos_brancos / eleitores * 100);
+
+
+if (votos_c1 > votos_c2 && votos_c1 > votos_c3){
+	
+	printf ("\nO vencedor das eleições é o candidato %s!", candidato_1 );
+}
+else if (votos_c2 > votos_c1 && votos_c2 > votos_c3){
+	
+	printf ("\nO vencedor das eleições é o candidato %s!", candidato_2);
+}
+	else {
+		printf ("\nO vencedor das eleições é o candidato %s!", candidato_3);
+	}
+return 0;
+
+
+	
+}
