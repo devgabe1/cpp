935c3afd1438a364693a29b451c5fcbae2151b92
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-04-03 20:13:04 -0300

    Adicionado exercício 7 da lista 02

--- src/lista_de_exercicios/lista_de_exercícios_2/07_apresentar_valores_divisiveis_por_2_ou_3.cpp
+++ src/lista_de_exercicios/lista_de_exercícios_2/07_apresentar_valores_divisiveis_por_2_ou_3.cpp
@@ -3,7 +3,7 @@ int main (){
 
 int valor_a, valor_b, valor_c, valor_d, valor_variavel;
 
-printf ("Apresentar valores divisíveis por 2 ou 3.");
+printf ("\nApresentar valores divisíveis por 2 ou 3.\n\n");
 
 printf ("Insira o valor de A: ");
 scanf ("%d", &valor_a);
@@ -17,8 +17,58 @@ scanf ("%d", &valor_c);
 printf ("Insira o valode de D: ");
 scanf ("%d", &valor_d);
 
-if (valor_a % 2 == 0 || valor_a % 3 == 0){
-    
+
+if (valor_a % 2 == 0) {
+    if (valor_a % 3 == 0){
+    printf ("\nO valor A é divisível por 2 e 3.");
+} else { printf ("\nO valor A é divisível apenas por 2.");
+}
+}
+else if (valor_a % 3 == 0){
+    printf ("\nO valor A é divisível apenas por 3.");
 }
 
+
+if (valor_b % 2 == 0){
+    if (valor_b % 3 == 0){
+    printf ("\nO valor B é divisível por 2 e 3.");
+} else { printf ("\nO valor B é divisível apenas por 2.");
 }
+}
+else if (valor_b % 3 == 0){
+    printf ("\nO valor B é divisível apenas por 3.");
+}
+
+
+if (valor_c % 2 == 0){
+    if (valor_c % 3 == 0){
+        printf ("\nO valor C é divisível por 2 e 3.");
+    } else { printf ("\nO valor C é divisível apenas por 2.");
+}
+}
+    else if (valor_c % 3 == 0){
+    printf ("\nO valor C é divisível apenas por 3.");
+    }
+
+
+if (valor_d % 2 == 0){
+    if (valor_d % 3 == 0){
+        printf ("\nO valor D é divisível por 2 e 3.");
+    } else { printf ("\nO valor D é divisível apenas por 2.");
+    }
+    }
+else if (valor_d % 3 == 0){
+    printf ("\nO valor D é divisível apenas por 3.");
+}
+}
+
+
+
+
+
+
+
+
+
+
+  
