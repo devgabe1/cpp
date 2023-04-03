af845c493a978df25bf414b933edee780560c6c1
Gabriel Schultz Vasconcelos <gabrielschultz2807@gmail.com>
2023-04-03 11:47:40 -0300

    Corrigido caracteres especiais

--- src/lista_de_exercicios/lista_de_exercícios_1/24_desafio_lista_complementar.cpp
+++ src/lista_de_exercicios/lista_de_exercícios_1/24_desafio_lista_complementar.cpp
@@ -12,42 +12,42 @@ int  alunos_direito, alunos_engenharia, alunos_computacao, total_alunos;
 float valor_engenharia, valor_direito, valor_computacao, total_valor, aa1;
 char faculdade[50];
 
-printf ("\nLevantamento arrecada��o e alunos de uma Faculdade\n\n\n");
+printf ("\nLevantamento arrecadação e alunos de uma Faculdade\n\n\n");
 printf ("Insira o nome da Faculdade: ");
 fgets(faculdade, 50, stdin);
 
 
 
-printf ("\nInsira o n�meros de alunos matr�culados no curso de Engenharia: ");
+printf ("\nInsira o números de alunos matrículados no curso de Engenharia: ");
 scanf ("%d", &alunos_engenharia);
 
 printf ("\nInsira o valor da mensalidade do curso de Engenharia R$: ");
 scanf ("%f", &valor_engenharia);
 
 
-printf ("\nInsira o n�mero de alunos matr�culados no curso de Direito: ");
+printf ("\nInsira o número de alunos matrículados no curso de Direito: ");
 scanf ("%d", &alunos_direito);
 
 printf ("\nInsira o valor da mensalidade do curso de Direito R$: ");
 scanf ("%f", &valor_direito);
 
 
-printf ("\nInsira o n�mero de alunos matr�culados no curso de Computa��o: ");
+printf ("\nInsira o número de alunos matrículados no curso de Computação: ");
 scanf ("%d", &alunos_computacao);
 
-printf ("\nInsira o valor da mensalidade do curso de Computa��o R$: ");
+printf ("\nInsira o valor da mensalidade do curso de Computação R$: ");
 scanf ("%f", &valor_computacao);
 
 total_alunos = alunos_engenharia + alunos_direito + alunos_computacao; 
 
-printf ("\n\nO percentual de alunos inscritos no curso de Engenharia �: %.2f%%\n", ((float)alunos_engenharia / (float)total_alunos) * 100);
-printf ("O valor total arrecadado por m�s pelo curso de Engenharia � R$:%'.2f\n\n", (float)valor_engenharia * (float)alunos_engenharia);
+printf ("\n\nO percentual de alunos inscritos no curso de Engenharia é: %.2f%%\n", ((float)alunos_engenharia / (float)total_alunos) * 100);
+printf ("O valor total arrecadado por mês pelo curso de Engenharia é R$:%'.2f\n\n", (float)valor_engenharia * (float)alunos_engenharia);
 
-printf ("O percentual de alunos inscritos no curso de Direito �: %.2f%%\n", ((float)alunos_direito / (float)total_alunos) * 100);
-printf ("O valor total arrecadado por m�s pelo curo de Direito � R$:%'.2f\n\n", valor_direito * (float)alunos_direito);
+printf ("O percentual de alunos inscritos no curso de Direito é: %.2f%%\n", ((float)alunos_direito / (float)total_alunos) * 100);
+printf ("O valor total arrecadado por mês pelo curo de Direito é R$:%'.2f\n\n", valor_direito * (float)alunos_direito);
  
-printf ("O percentual de alunos inscritos no curo de Computa��o �: %.2f%%\n", ((float)alunos_computacao / (float)total_alunos) * 100);
-printf ("O valor total arrecadado por m�s pelo curso de Computa��o � R$:""%'.2f\n\n", valor_computacao * (float)alunos_computacao);
+printf ("O percentual de alunos inscritos no curo de Computação é: %.2f%%\n", ((float)alunos_computacao / (float)total_alunos) * 100);
+printf ("O valor total arrecadado por mês pelo curso de Computação é R$:""%'.2f\n\n", valor_computacao * (float)alunos_computacao);
 
 
 total_valor = (valor_engenharia + valor_direito + valor_computacao) * total_alunos;
@@ -59,7 +59,7 @@ printf("\n\nO valor total arrecadado pela Faculdade %s", faculdade);
 
 
 setlocale(LC_ALL, "portuguese");
-printf ("no per�odo de um ano � R$""%'.2f", total_valor * 12);
+printf ("no período de um ano é R$""%'.2f", total_valor * 12);
 
 
 }
