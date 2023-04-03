bf2e12cf2eac8498d71541bca57ce742d522a322
Gabriel Schultz Vasconcelos <gabrielschultz2807@gmail.com>
2023-04-03 16:53:12 -0300

    Adicionado exercício 07 da lista 02

---
+++ src/lista_de_exercicios/lista_de_exercícios_2/07_apresentar_valores_divisiveis_por_2_ou_3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (){
+
+int valor_a, valor_b, valor_c, valor_d, valor_variavel;
+
+printf ("Apresentar valores divisíveis por 2 ou 3.");
+
+printf ("Insira o valor de A: ");
+scanf ("%d", &valor_a);
+
+printf ("Insira o valor de B: ");
+scanf ("%d", &valor_b);
+
+printf ("Insira o valor de C: ");
+scanf ("%d", &valor_c);
+
+printf ("Insira o valode de D: ");
+scanf ("%d", &valor_d);
+
+if (valor_a % 2 == 0 || valor_a % 3 == 0){
+    
+}
+
+}
