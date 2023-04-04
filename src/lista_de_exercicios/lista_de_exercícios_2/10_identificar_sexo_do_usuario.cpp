8793c472702b4de479a49810bbe6d82b44dc38b4
Gabriel Schultz Vasconcelos <gabrielschultz2807@gmail.com>
2023-04-04 11:50:25 -0300

    Adicionado exercício 10 da lista 02

--- src/lista_de_exercicios/lista_de_exercícios_2/10_identificar_sexo_do_usuario.cpp
+++ src/lista_de_exercicios/lista_de_exercícios_2/10_identificar_sexo_do_usuario.cpp
@@ -9,12 +9,21 @@ printf ("\nIdentificar o sexo do usuário.\n\n");
 printf ("Insira seu nome: ");
 fgets (nome, 50, stdin);
 
+nome[strcspn(nome, "\n")] = 0; //Essa função retorna o índice do primeiro caractere em uma string que aparece em outra string especificada.
+
 printf ("\nInsira o seu sexo: ");
 scanf ("%c", &sexo);
 
-while (sexo == 'm') {
+while (sexo != 'M' && sexo != 'F') {
     printf ("\nGentileza preencher o campo sexo com M ou F.");
     printf ("\n\nInsira o seu sexo: ");
-    scanf ("%c", &sexo);
+    scanf (" %c", &sexo);
+}
+
+if (sexo == 'M'){
+    printf ("Ilmo. Sr. %s.\n", nome);
+} else {
+    printf ("\nIlma. Sra. %s.\n", nome);
 }
 }
+
