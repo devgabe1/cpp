565c5f52d4d55266601eedbb085ddcebe516cf15
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-04-25 22:33:10 -0300

    Adicionado exercício 01 da lista 05

---
+++ src/lista_de_exercicios/lista_de_exercicios_05/01_apresentar_valores_invertendo_a_ordem_de_entrada.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(){
+
+float v_01, v_02, v_03, v_04, v_05, v_06, v_07, v_08, v_09, v_10;
+
+printf ("\nApresentar valores na ordem inversa da entrada.\n\n");
+printf ("Insira o valor 01: ");
+scanf ("%f", &v_01);
+
+printf ("\nInsira o valor 02: ");
+scanf ("%f", &v_02);
+
+printf ("\nInsira o valor 03: ");
+scanf ("%f", &v_03);
+
+printf ("\nInsira o valor 04: ");
+scanf ("%f", &v_04);
+
+printf ("\nInsira o valor 05: ");
+scanf ("%f", &v_05);
+
+printf ("\nInsira o valor 06: ");
+scanf ("%f", &v_06);
+
+printf ("\nInsira o valor 07: ");
+scanf ("%f", &v_07);
+
+printf ("\nInsira o valor 08: ");
+scanf ("%f", &v_08);
+
+printf ("\nInsira o valor 09: ");
+scanf ("%f", &v_09);
+
+printf ("\nInsira o valor 10: ");
+scanf ("%f", &v_10);
+
+
+printf ("\n\nValores:\n10 - %.2f", v_10);
+printf ("\n09 - %.2f", v_09);
+printf ("\n08 - %.2f", v_08);
+printf ("\n07 - %.2f", v_07);
+printf ("\n06 - %.2f", v_06);
+printf ("\n05 - %.2f", v_05);
+printf ("\n04 - %.2f", v_04);
+printf ("\n03 - %.2f", v_03);
+printf ("\n02 - %.2f", v_02);
+printf ("\n01 - %.2f", v_01);
+
+return 0;
+} 
